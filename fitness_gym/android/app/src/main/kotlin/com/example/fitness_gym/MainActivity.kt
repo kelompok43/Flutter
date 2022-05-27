a768f86e8cc97af672061d139cfeefcc4c242f31
MZannn <81086117+MZannn@users.noreply.github.com>
2022-05-27 19:24:04 +0700

    Delete fitness_gym directory

--- fitness_gym/android/app/src/main/kotlin/com/example/fitness_gym/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.fitness_gym
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- fitness_gym/lib/screens/register/register_screen_view_model.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/cupertino.dart';
-
-class RegisterViewModel extends ChangeNotifier{
-  var isHidden = false;
-  void obscureText(bool isHidden){
-    isHidden != isHidden;
-    notifyListeners();
-  }
-}

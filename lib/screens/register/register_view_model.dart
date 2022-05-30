cf6ac02fc27eefd0f55c6de204a2592acb1ae0a3
MZannn <mfauzan1098@gmail.com>
2022-05-30 18:40:37 +0700

    Memperbaiki Register View Model

--- lib/screens/register/register_view_model.dart
+++ lib/screens/register/register_view_model.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/cupertino.dart';
 
 class RegisterViewModel extends ChangeNotifier{
-  bool _passIsHidden = true;
+  bool _passIsHidden = false;
   bool get passIsHidden => _passIsHidden;
   set passisHidden(bool newValue) {
     _passIsHidden = newValue;
     notifyListeners();
   }
-  bool _confirmPassIsHidden = true;
+  bool _confirmPassIsHidden = false;
   bool get confirmPassIsHidden => _confirmPassIsHidden;
   set confirmPassIsHidden(bool newValue) {
     _confirmPassIsHidden = newValue;

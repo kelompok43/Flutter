5db19a931e99831e94cd1982ad79d7d7b33c5595
MZannn <mfauzan1098@gmail.com>
2022-06-03 19:56:28 +0700

    Menambahkan Color Global

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,4 +1,78 @@
 import 'package:flutter/material.dart';
 
-var colorOrange = const Color(0xFFF48743);
-var colorGrey = const Color(0xB3B3B3B3);
+// Color Primary
+Color primary1 = const Color(0xFFFEE9CC);
+Color primary2 = const Color(0xFFFDCD9B);
+Color primary3 = const Color(0xFFF9A968);
+Color primary4 = const Color(0xFFF48743);
+Color primary5 = const Color(0xFFED5108);
+Color primary6 = const Color(0xFFCB3805);
+Color primary7 = const Color(0xFFAA2304);
+Color primary8 = const Color(0xFF891302);
+Color primary9 = const Color(0xFF710701);
+
+// Color Secondary
+Color secondary1 = const Color(0xFFCFD0D0);
+Color secondary2 = const Color(0xFFB0B0B1);
+Color secondary3 = const Color(0xFF88898A);
+Color secondary4 = const Color(0xFF606162);
+Color secondary5 = const Color(0xFF393A3B);
+Color secondary6 = const Color(0xFF111214);
+Color secondary7 = const Color(0xFF0E0F11);
+Color secondary8 = const Color(0xFF0B0C0D);
+Color secondary9 = const Color(0xFF09090A);
+
+// Color Success
+Color success1 = const Color(0xFFECFBD2);
+Color success2 = const Color(0xFFD5F8A7);
+Color success3 = const Color(0xFFB3EA78);
+Color success4 = const Color(0xFF8FD653);
+Color success5 = const Color(0xFF60BC23);
+Color success6 = const Color(0xFF47A119);
+Color success7 = const Color(0xFF328711);
+Color success8 = const Color(0xFF206D0B);
+Color success9 = const Color(0xFF145A06);
+
+// Color Danger
+Color danger1 = const Color(0xFFFEDFD9);
+Color danger2 = const Color(0xFFFEB8B4);
+Color danger3 = const Color(0xFFFE8E93);
+Color danger4 = const Color(0xFFFD7285);
+Color danger5 = const Color(0xFFFC446F);
+Color danger6 = const Color(0xFFD83168);
+Color danger7 = const Color(0xFFB52260);
+Color danger8 = const Color(0xFF921556);
+Color danger9 = const Color(0xFF780D4F);
+
+// Color Warning
+Color warning1 = const Color(0xFFFFF1AA);
+Color warning2 = const Color(0xFFFFE980);
+Color warning3 = const Color(0xFFFFE255);
+Color warning4 = const Color(0xFFFFDB2B);
+Color warning5 = const Color(0xFFFFD400);
+Color warning6 = const Color(0xFFD4B100);
+Color warning7 = const Color(0xFFAA8D00);
+Color warning8 = const Color(0xFF806A00);
+Color warning9 = const Color(0xFF806A00);
+
+// Color Info
+Color info1 = const Color(0xFFCCFEFA);
+Color info2 = const Color(0xFF99FDFD);
+Color info3 = const Color(0xFF66EEF9);
+Color info4 = const Color(0xFF40D9F4);
+Color info5 = const Color(0xFF04BAED);
+Color info6 = const Color(0xFF0291CB);
+Color info7 = const Color(0xFF026DAA);
+Color info8 = const Color(0xFF014D89);
+Color info9 = const Color(0xFF003871);
+
+// Color Neutral
+Color neutral1 = const Color(0xFFF4F4F4);
+Color neutral2 = const Color(0xFFEDEDED);
+Color neutral3 = const Color(0xFFDEDEDE);
+Color neutral4 = const Color(0xFFBBBBBB);
+Color neutral5 = const Color(0xFF9C9C9C);
+Color neutral6 = const Color(0xFF737373);
+Color neutral7 = const Color(0xFF5F5F5F);
+Color neutral8 = const Color(0xFF404040);
+Color neutral9 = const Color(0xFF202020);

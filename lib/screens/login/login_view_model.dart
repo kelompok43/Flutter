a04c3de068be6277b52e9b58bb51fbf675759cff
MZannn <mfauzan1098@gmail.com>
2022-06-14 23:38:49 +0700

    Membuat Login View Model

--- lib/screens/login/login_view_model.dart
+++ lib/screens/login/login_view_model.dart
@@ -1,13 +1,12 @@
 import 'package:dio/dio.dart';
 import 'package:fitness_gym/models/api/user_api.dart';
 import 'package:fitness_gym/screens/dashboard/dashboard_screen.dart';
+import 'package:fitness_gym/utils/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class LoginViewModel extends ChangeNotifier {
-  var loginData = '';
   bool _isHidden = true;
-  var url = "http://54.227.80.34/api/login/";
   bool get isHidden => _isHidden;
   set isHidden(bool newValue) {
     _isHidden = newValue;
@@ -21,9 +20,12 @@ class LoginViewModel extends ChangeNotifier {
       String _token = loginUser['data']['token'];
       final sharedPref = await SharedPreferences.getInstance();
       await sharedPref.setString('token', _token);
-      print(_token);
-      const snackBar = SnackBar(
-        content: Text('Login Berhasil'),
+      print(sharedPref.getString("token"));
+      SnackBar snackBar = SnackBar(
+        behavior: SnackBarBehavior.floating,
+        backgroundColor: primary5,
+        duration: const Duration(seconds: 2),
+        content: const Text('Login Berhasil'),
       );
       ScaffoldMessenger.of(context).showSnackBar(snackBar);
       Navigator.pushAndRemoveUntil(
@@ -34,8 +36,11 @@ class LoginViewModel extends ChangeNotifier {
           (route) => false);
     } on DioError catch (e) {
       if (e.response!.statusCode == 400) {
-        const snackBar = SnackBar(
-          content: Text('Login Gagal'),
+        SnackBar snackBar = SnackBar(
+          behavior: SnackBarBehavior.floating,
+          backgroundColor: primary5,
+          duration: const Duration(seconds: 2),
+          content: const Text('Login Gagal'),
         );
         ScaffoldMessenger.of(context).showSnackBar(snackBar);
       }
